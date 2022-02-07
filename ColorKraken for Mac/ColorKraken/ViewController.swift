3df99424ecfc42a0ad3b081cc7d0f622f8d34518
Bruce <bruce.gomes@intellitect.com>
2022-02-07 00:38:28 -0800

    empty initial project setup
    stubbing out very initial project

---
+++ ColorKraken for Mac/ColorKraken/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ColorKraken
+//
+//  Created by Bruce Gomes on 2/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    @IBOutlet weak var tableView: NSTableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
