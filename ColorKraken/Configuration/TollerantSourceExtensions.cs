1b190dbd5865b6963c4180cc511911a6753b1559
Kevin Bost <kitokeboo@gmail.com>
2022-02-16 13:38:07 -0800

    Adding KV settings and IConfiguration

---
+++ ColorKraken/Configuration/TollerantSourceExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace ColorKraken.Configuration;
+
+public static class TollerantSourceExtensions
+{
+    public static void IgnoreExceptionFromLastSource(this IConfigurationBuilder builder)
+    {
+        var last = builder.Sources[builder.Sources.Count - 1];
+        builder.Sources.RemoveAt(builder.Sources.Count - 1);
+        builder.Add(new TollerantSource(last));
+    }
+}
