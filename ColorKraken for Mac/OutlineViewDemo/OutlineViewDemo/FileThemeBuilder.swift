751259cea56088c6bf8011adcd29f62df1da0968
Bruce <bruce.gomes@intellitect.com>
2022-03-14 00:06:50 -0700

    bringing old Themebuilder back

---
+++ ColorKraken for Mac/OutlineViewDemo/OutlineViewDemo/FileThemeBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  FileThemeBuilder.swift
+//  OutlineViewDemo
+//
+//  Created by Bruce Gomes on 3/13/22.
+//  Copyright © 2022 Appcoda. All rights reserved.
+//
+
+import Foundation
---
+++ ColorKraken for Mac/OutlineViewDemo/OutlineViewDemo/ThemeBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  ThemeBuilder.swift
+//  OutlineViewDemo
+//
+//  Created by Bruce Gomes on 3/14/22.
+//  Copyright © 2022 Appcoda. All rights reserved.
+//
+
+import Foundation
