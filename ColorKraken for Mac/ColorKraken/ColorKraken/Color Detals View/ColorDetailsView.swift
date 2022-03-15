7eea7eee693612e5e65a67b82a0e26f2acacf874
Bruce <bruce.gomes@intellitect.com>
2022-03-15 00:11:15 -0700

    color label fix

--- ColorKraken for Mac/ColorKraken/ColorKraken/Color Detals View/ColorDetailsView.swift
+++ ColorKraken for Mac/ColorKraken/ColorKraken/Color Detals View/ColorDetailsView.swift
@@ -52,9 +52,9 @@ class ColorDetailsView: NSView {
     
     @IBAction func showColorsPanel(_ sender: Any) {
         redLabel.stringValue = String(format: "Red: %.3f", colorWell.color.redComponent)
-        greenLabel.stringValue = String(format: "Red: %.3f", colorWell.color.greenComponent)
-        blueLabel.stringValue = String(format: "Red: %.3f", colorWell.color.blueComponent)
-        alphaLabel.stringValue = String(format: "Red: %.2f", colorWell.color.alphaComponent)
+        greenLabel.stringValue = String(format: "Green: %.3f", colorWell.color.greenComponent)
+        blueLabel.stringValue = String(format: "Blue: %.3f", colorWell.color.blueComponent)
+        alphaLabel.stringValue = String(format: "Alpha: %.2f", colorWell.color.alphaComponent)
         
         delegate?.shouldUpdateColor(withRed: colorWell.color.redComponent,
                                     green: colorWell.color.greenComponent,
@@ -78,9 +78,9 @@ class ColorDetailsView: NSView {
     func set(color: Color) {
         colorWell.color = color.toNSColor()
         redLabel.stringValue = String(format: "Red: %.3f", color.red)
-        greenLabel.stringValue = String(format: "Red: %.3f", color.green)
-        blueLabel.stringValue = String(format: "Red: %.3f", color.blue)
-        alphaLabel.stringValue = String(format: "Red: %.2f", color.alpha)
+        greenLabel.stringValue = String(format: "Green: %.3f", color.green)
+        blueLabel.stringValue = String(format: "Blue: %.3f", color.blue)
+        alphaLabel.stringValue = String(format: "Alpha: %.2f", color.alpha)
     }
  
     
